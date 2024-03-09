37a126f6053e31006b0beeeadeaaa515211ffc45
aelew <118043566+aelew@users.noreply.github.com>
2024-03-09 13:52:10 -0800

    add search bar functionality

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,12 +1,50 @@
+import { useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
 import search from '../assets/search.png';
 
-const Search = () => {
-  return (
-    <form action="" className="search-bar">
-      <input type="text" placeholder="Your Location..." />
-      <input type="image" src={search} width="25px" />
-    </form>
-  );
-};
+export default function Search() {
+  const inputRef = useRef();
+  const map = useMap();
 
-export default Search;
+  const places = useMapsLibrary('places');
+  const [placeAutocomplete, setPlaceAutocomplete] = useState(null);
+
+  const handlePlaceUpdate = useCallback(
+    (place) => {
+      console.log('place updated', place);
+      if (place.geometry?.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      }
+    },
+    [map]
+  );
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ['geometry', 'name', 'formatted_address']
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places, inputRef]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+    placeAutocomplete.addListener('place_changed', () => {
+      const place = placeAutocomplete.getPlace();
+      handlePlaceUpdate(place);
+    });
+  }, [placeAutocomplete, handlePlaceUpdate]);
+
+  if (!map) {
+    return null;
+  }
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Enter a location" ref={inputRef} />
+      <input type="image" src={search} width="25px" />
+    </div>
+  );
+}

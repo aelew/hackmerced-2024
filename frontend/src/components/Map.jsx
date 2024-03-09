996b87e2c24d7e4b55226a14abb2e2a4e15d0366
aelew <118043566+aelew@users.noreply.github.com>
2024-03-08 21:32:02 -0800

    add map component

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,48 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useCallback, useState } from 'react';
+
+const containerStyle = {
+  width: '100%',
+  height: '80vh',
+  borderRadius: '0.5rem'
+};
+
+const center = {
+  lat: 37.3647,
+  lng: -120.4241
+};
+
+export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  if (!isLoaded) {
+    return <p>Loading map...</p>;
+  }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+    </GoogleMap>
+  );
+}

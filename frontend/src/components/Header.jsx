48d3e863fcc76c82381c913e893fdb6010df7fb0
aelew <118043566+aelew@users.noreply.github.com>
2024-03-09 20:42:28 -0800

    header improvements

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,51 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { TbHealthRecognition } from 'react-icons/tb';
 
-import menu from '../assets/menu.png';
-import person from '../assets/person-icon.png';
-import search from '../assets/search.png';
 import Search from './Search.jsx';
 
 const Header = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
   return (
     <div className="header-box">
       <div className="header">
-        <h1>Health Map</h1>
+        <div className="header-logo">
+          <TbHealthRecognition size={32} />
+          <h1>Health Map</h1>
+        </div>
         <Search />
         <nav className="nav-bar">
-          <button
-            onClick={() =>
-              isAuthenticated
-                ? logout({ logoutParams: { returnTo: window.location.origin } })
-                : loginWithRedirect()
-            }
+          <div
             style={{
-              backgroundColor: 'transparent',
-              cursor: 'pointer',
-              fontSize: '1rem',
-              border: 'none'
+              display: 'flex',
+              alignItems: 'end',
+              flexDirection: 'column'
             }}
           >
-            {isAuthenticated ? 'Log out' : 'Log in'}
-          </button>
-          <img src={person} width="25px" />
-          <img src={menu} width="25px" />
+            {user && (
+              <span style={{ color: '#000', fontWeight: 500 }}>
+                {user.name ?? user.nickname}
+              </span>
+            )}
+            <button
+              onClick={() =>
+                isAuthenticated
+                  ? logout({
+                      logoutParams: { returnTo: window.location.origin }
+                    })
+                  : loginWithRedirect()
+              }
+              style={{
+                backgroundColor: 'transparent',
+                textAlign: 'right',
+                marginLeft: 'auto',
+                cursor: 'pointer',
+                fontSize: '1rem',
+                border: 'none'
+              }}
+            >
+              {user ? <small>Log out</small> : <p>Log in with Auth0</p>}
+            </button>
+          </div>
         </nav>
       </div>
     </div>

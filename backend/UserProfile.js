30cf49c54def61950ddbe25b788e1ef90908ce31
Aditya <raoadityacodes@gmail.com>
2024-03-09 19:19:17 -0800

    updated auth0 category, not fully functional yet

--- backend/UserProfile.js
+++ backend/UserProfile.js
@@ -3,17 +3,25 @@ const mongoose = require('mongoose');
 const UserProfileSchema = new mongoose.Schema({
   // data fields for the user profile
   // every user profile will have a username, and a boolean for each of the following fields
-  userName: String,
-  userAllergy: boolean,
-  userRespiratory: boolean,
-  userImmuneSystem: boolean,
+  auth0Id: { 
+    type: String,
+    required: true,
+    unique: true,
+  },
+  vulnerabilities:{
+    allergy: Boolean,
+    respiratory: Boolean,
+    immuneSystem: Boolean
+  },
 
+  tolerance:{
   // personal tolerances
   pollenTolerance: Number,
   airQualityTolerance: Number,
   radiationTolerance: Number,
   covidTolerance: Number,
   fluTolerance: Number
+  }
 });
 
 const UserProfile = mongoose.model('UserProfile', UserProfileSchema);

fba8a40ed733472943e7a3fb343e379296ff37ea
aelew <118043566+aelew@users.noreply.github.com>
2024-03-09 19:02:15 -0800

    add aqi data to place card

--- frontend/src/components/PlaceCard.jsx
+++ frontend/src/components/PlaceCard.jsx
@@ -3,15 +3,33 @@ import { useEffect, useState } from 'react';
 import { useAppContext } from '../AppContext';
 
 export default function PlaceCard() {
+  const [airQualityIndexes, setAirQualityIndexes] = useState([]);
   const [pollenForecast, setPollenForecast] = useState([]);
   const { place } = useAppContext();
 
   useEffect(() => {
     if (place) {
+      const latitude = place.geometry.location.lat();
+      const longitude = place.geometry.location.lng();
+
+      // Fetch current AQI conditions
+      fetch(
+        `https://airquality.googleapis.com/v1/currentConditions:lookup?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`,
+        {
+          body: JSON.stringify({ location: { latitude, longitude } }),
+          headers: { 'Content-Type': 'application/json' },
+          method: 'POST'
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => setAirQualityIndexes(data.indexes))
+        .catch((err) => console.error('failed to fetch aqi data:', err));
+
+      // Fetch pollen forecast data
       const url = new URL('https://pollen.googleapis.com/v1/forecast:lookup');
       url.searchParams.set('key', import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
-      url.searchParams.set('location.longitude', place.geometry.location.lng());
-      url.searchParams.set('location.latitude', place.geometry.location.lat());
+      url.searchParams.set('location.latitude', latitude);
+      url.searchParams.set('location.longitude', longitude);
       url.searchParams.set('days', 5);
       fetch(url)
         .then((res) => res.json())
@@ -32,10 +50,43 @@ export default function PlaceCard() {
       </div>
       <div className="place-card-section">
         <div>
+          <h2>Current air quality conditions</h2>
+          <p>Air quality data is updated hourly.</p>
+        </div>
+        <div className="place-card-data">
+          {airQualityIndexes.map((aqi) => (
+            <div className="place-sub-card" key={aqi.code}>
+              <div className="place-sub-card-header">
+                <strong>{aqi.displayName}</strong>
+              </div>
+              <div
+                style={{
+                  alignItems: 'center',
+                  display: 'flex',
+                  gap: '0.25rem'
+                }}
+              >
+                <strong>{aqi.aqi}</strong> - {aqi.category}{' '}
+                <div
+                  style={{
+                    backgroundColor: `rgb(${aqi.color.red * 255}, ${aqi.color.green * 255}, ${aqi.color.blue * 255})`,
+                    borderRadius: '0.25rem',
+                    height: '16px',
+                    width: '16px'
+                  }}
+                />
+              </div>
+              <p>Dominant pollutant: {aqi.dominantPollutant.toUpperCase()}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="place-card-section">
+        <div>
           <h2>Pollen Forecast</h2>
           <p>Daily pollen information for the next 5 days</p>
         </div>
-        <div className="pollen-forecasts">
+        <div className="place-card-data">
           {pollenForecast.map((forecast) => (
             <div
               key={`${forecast.date.year}+${forecast.date.month}+${forecast.date.day}`}
@@ -44,10 +95,10 @@ export default function PlaceCard() {
               <h3 style={{ fontSize: '1.125rem' }}>
                 {forecast.date.month}/{forecast.date.day}/{forecast.date.year}
               </h3>
-              <div className="pollen-info">
+              <div className="place-card-section-data">
                 {forecast.pollenTypeInfo.map((pollenType) => (
-                  <div className="pollen-type-card" key={pollenType.code}>
-                    <div className="pollen-type-card-header">
+                  <div className="place-sub-card" key={pollenType.code}>
+                    <div className="place-sub-card-header">
                       <strong>{pollenType.displayName}</strong>
                       {pollenType.inSeason ? (
                         <span style={{ color: '#f59e0b' }}>In season</span>

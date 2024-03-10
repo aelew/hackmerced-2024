f02e2f231410ea5c5bf32329a830a4bad8470972
aelew <118043566+aelew@users.noreply.github.com>
2024-03-09 16:07:45 -0800

    pollen api heatmap

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -5,9 +5,10 @@ import {
   InfoWindow,
   MapControl,
   Marker,
-  useAdvancedMarkerRef
+  useAdvancedMarkerRef,
+  useMap
 } from '@vis.gl/react-google-maps';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useAppContext } from '../AppContext';
 
@@ -16,53 +17,24 @@ const defaultPosition = {
   lng: -120.4241
 };
 
-const createTileUrlFunction = () => {
-  return (coord, zoom) => {
-    const normalizedCoord = getNormalizedCoord(coord, zoom);
-    if (!normalizedCoord) {
-      return null;
-    }
-    const bound = Math.pow(2, zoom);
-    return `https://pollen.googleapis.com/v1/mapTypes/TREE_UPI/heatmapTiles/${1}/${1}/${1}?key=AIzaSyCe5jXBDvA9rj5X-m0T11rH2rtylLRdZH0`;
-  };
-};
-
-// Normalizes the coordinates for tiles that wrap around the globe
-const getNormalizedCoord = (coord, zoom) => {
-  const y = coord.y;
-  let x = coord.x;
-  const tileRange = 1 << zoom;
-
-  // Wrap tiles horizontally
-  if (x < 0 || x >= tileRange) {
-    x = ((x % tileRange) + tileRange) % tileRange;
-  }
-
-  if (y < 0 || y >= tileRange) {
-    return null;
-  }
-
-  return { x, y };
-};
-
 export default function Map() {
+  const map = useMap();
   const { place } = useAppContext();
 
   // for our UC merced marker
   const [infowindowOpen, setInfowindowOpen] = useState(true);
   const [markerRef, marker] = useAdvancedMarkerRef();
 
-  // useEffect(() => {
-  //   if (isLoaded && map) {
-  //     const tileUrlFunction = createTileUrlFunction();
-  //     const coordMapType = new window.google.maps.ImageMapType({
-  //       getTileUrl: tileUrlFunction,
-  //       tileSize: new window.google.maps.Size(256, 256)
-  //     });
-
-  //     map.overlayMapTypes.push(coordMapType);
-  //   }
-  // }, [isLoaded, map]);
+  useEffect(() => {
+    if (map) {
+      const imageMapType = new window.google.maps.ImageMapType({
+        tileSize: new window.google.maps.Size(256, 256),
+        getTileUrl: (coord, zoom) =>
+          `https://pollen.googleapis.com/v1/mapTypes/TREE_UPI/heatmapTiles/${zoom}/${coord.x}/${coord.y}?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`
+      });
+      map.overlayMapTypes.push(imageMapType);
+    }
+  }, [map]);
 
   return (
     <div className="map-container">
@@ -70,8 +42,8 @@ export default function Map() {
         mapId="e539c9b65757ae2"
         className="map"
         defaultZoom={10}
-        defaultCenter={defaultPosition}
         fullscreenControl={false}
+        defaultCenter={defaultPosition}
       >
         {/* Child components, such as markers, info windows, etc. go in here */}
         <Marker position={defaultPosition} />
